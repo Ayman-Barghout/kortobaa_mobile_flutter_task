6c8efbcdfa7ef0a12333977f993869679e51e45d
Ayman-Barghout <ayman.a.barghout@gmail.com>
2019-12-18 15:11:31 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/kortobaa/kortobaa_mobile_flutter_task/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kortobaa.kortobaa_mobile_flutter_task
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
